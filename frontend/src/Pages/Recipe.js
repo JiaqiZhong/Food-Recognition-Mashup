c289d69e55871df9e6f4f6373c49e20f88957137
JiaqiZhong <n10658327@qut.edu.au>
2024-05-20 13:25:22 +1000

    Construct and store all the necessary information of recipes fetched from spoonacular api that can be used by both recipes list page and recipe details page in recipeContent json file for later use

---
+++ frontend/src/Pages/Recipe.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useLocation, Link, useNavigate } from 'react-router-dom';
+import './Recipe.css';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Row, Col } from 'react-bootstrap';
+import data from '../JSON/predictionsWithNutritionFacts';
+
+function Recipe() {
+    //const { recipeID } = useParams();  
+    const [loading, setLoading] = useState(true);
+    const [recipe, setRecipe] = useState([]);
+    const hasLoadedBefore = useRef(true)
+
+    const location = useLocation();
+    const ingredients = location.state.ingredients;
+
+    // useEffect(() => {
+    //     if(hasLoadedBefore.current) {
+    //       console.log("Effect ran")
+    //       hasLoadedBefore.current = false;
+    //     }
+    //     else {
+    //       console.log('component rendered');
+    //       getRecipe(`http://localhost:4000/recipes/${recipeID}`);
+    //     }
+    //   }, []);
+
+    // function getRecipe(url) {
+    //     axios.get(`http://localhost:4000/modules/${recipeId}`)
+    //     .then((res) => {
+    //         let recipes = res.data;
+    //         const promises = recipes.map(async (recipe) => {
+    //             try {
+    //                 const res = await axios.get(`http://localhost:4000/recipes/${recipe.id}`);
+    //                 recipe.prepTime = res.data.readyInMinutes;
+    //                 recipe.servings = res.data.servings;
+    //                 recipe.cuisineType = res.data.cuisines;
+    //                 recipe.diets = res.data.diets;
+    //                 recipe.calories = res.data.nutrition.nutrients[1].amount;
+    //             } catch (err) {
+    //                 console.log(err);
+    //             }                  
+    //         });
+    //         Promise.all(promises)
+    //         .then(() => {
+    //           console.log(recipes);
+    //           setRecipes(recipes);
+    //           setLoading(false);
+    //         })
+    //         .catch(err => {
+    //           console.log(err);
+    //           setLoading(false);
+    //       });
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //         setLoading(false);
+    //     })
+    // }
+
+    return (
+        <div className="recipes">
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <div>
+
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Recipe;
