4940679c2e233531659acaf68bead2efd7114829
JiaqiZhong <n10658327@qut.edu.au>
2024-07-22 14:04:41 +1000

    Update react app build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-import logo from './logo.svg';
 
 import Landing from './Pages/Landing';
 import Preview from './Pages/Preview';
