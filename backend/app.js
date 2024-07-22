40b03f98e3984dbbfa4d414fdda894483bdd9fae
JiaqiZhong <n10658327@qut.edu.au>
2024-07-22 15:34:39 +1000

    update cors configuration

--- backend/app.js
+++ backend/app.js
@@ -13,18 +13,29 @@ const recipesRouter = require('./routes/recipes');
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:3000', // For local development
+  'https://food-lens-client.vercel.app' // For production
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+};
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json())
 app.use(cookieParser());
 app.use(cors());
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
