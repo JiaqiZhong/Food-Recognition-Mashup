116594445c44b33934980d99ac544b572059e646
JiaqiZhong <n10658327@qut.edu.au>
2024-07-29 11:23:44 +1000

    Update error handler

--- backend/app.js
+++ backend/app.js
@@ -50,20 +50,20 @@ app.use('/recognition', clarifaiRouter);
 app.use('/nutrition-facts', nutritionFactsRouter);
 app.use('/recipes', recipesRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  res.status(err.status || 500);
+  res.json({
+      message: err.message,
+      error: req.app.get('env') === 'development' ? err : {}
+  });
 });
 
 app.listen(port, () => {
